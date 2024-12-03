6c0fd808f8e50a49279a937e6c00830e82c6274d
Yoav Ganabr <yoav.ganbar@gmail.com>
2024-12-03 14:53:18 +0200

    initial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,27 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import Image from 'next/image';
+import styles from './page.module.css';
+
+import { css, styled } from '@juxio/react-styled';
+
+const titleStyles = css({
+  color: '#058FF2',
+  cursor: 'pointer',
+  '&:hover': {
+    color: '#0A4BCD',
+  },
+});
+
+const List = styled('ul', {
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    gap: '10px',
+    listStyle: 'inside',
+    '& li': {
+      color: '#616161',
+    },
+  },
+});
 
 export default function Home() {
   return (
@@ -7,89 +29,49 @@ export default function Home() {
       <main className={styles.main}>
         <Image
           className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
+          src="/logo.svg"
+          alt="Jux.io logo"
           width={180}
           height={38}
           priority
         />
-        <ol>
+        <p>
+          <span className={titleStyles}>
+            Hello from Jux! See code in <code>src/app/page.tsx</code>
+            {` `}
+          </span>
+          <span
+            className={css({
+              color: '#474747',
+            })}
+          >
+            to change the styles...
+          </span>
+        </p>
+        <List>
           <li>
-            Get started by editing <code>src/app/page.tsx</code>.
+            Write CSS in JS, with zero runtime - all styles are extracted and
+            calculated at build time
           </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
+          <li>
+            Full support for design tokens and themes, using
+            @juxio/design-tokens
+          </li>
+          <li>
+            Utility first - write utilities which can then be used to style your
+            components
+          </li>
+          <li>
+            CLI for easy management of your design tokens, themes, utilities and
+            styles
+          </li>
+          <li>
+            Type-safe - autocompletion for your design tokens, breakpoints, and
+            utilities
+          </li>
+          <li>Full interpolation support when generating styles</li>
+        </List>
       </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
